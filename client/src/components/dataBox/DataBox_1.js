0a833f1a8fe9784b441690a5573fa160895160db
lafry5 <lafry5@yahoo.com>
2020-11-01 13:01:44 -0700

    test

--- client/src/components/dataBox/DataBox_1.js
+++ client/src/components/dataBox/DataBox_1.js
@@ -4,44 +4,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import DataBox3 from './DataBox_3';
 
 function dataBox1() {
-
-  const [data, setData] = useState({ hits: [] });
+  
+  <h1>Total Cases</h1> 
+  // const [data, setData] = useState({ hits: [] });
  
-  useEffect(async () => {
+  // useEffect(async () => {
 
-    const result = await axios(
-      'https://api.covidtracking.com/v1/us/current.json',
-    );
+  //   const result = await axios(
+  //     'https://api.covidtracking.com/v1/us/current.json',
+  //   );
  
-    setData(result.data);
+  //   setData(result.data);
     
-    return (
-      <div class="card">
-      <ul>
-            {data.hits.map(item => (
-              <li key={item.objectID}>
-                <a href={item.url}>{item.title}</a>
-              </li>
-            ))}
-          </ul>
-      </div> 
-      )
-      }
-      export default dataBox1;
+  //   return (
+  //     <div class="card">
+  //     <ul>
+  //           {data.hits.map(item => (
+  //             <li key={item.objectID}>
+  //               <a href={item.url}>{item.title}</a>
+  //             </li>
+  //           ))}
+  //         </ul>
+  //     </div> 
+  //     )
+  //     }
 
-// -----------------------------------------------------------------------------------------------------------
-// The starting div in this commented code, is in line with the div above
-// -----------------------------------------------------------------------------------------------------------
-/* 
-<div class="card">
-  <h4 class="card-header bg-dark text-light">
-  </h4>
-  <ul id="list-toDo" class="list-group list-group-flush">
-    <li class="list-group-item">
-      <span class="badge badge-primary badge-pill"></span>
-      <p class="m-1"></p>
-    </li>
-  </ul>
-</div> 
-*/
-// -----------------------------------------------------------------------------------------------------------
+  // )};
+
+};
+
+     export default dataBox1;
