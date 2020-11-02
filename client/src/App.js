b4d6b77dfe8d7c8f9c5d134e7b15697d21417edf
lafry5 <lafry5@yahoo.com>
2020-11-01 17:24:54 -0700

    fixes to src folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,86 +1,24 @@
-import DataBox from './components/dataBox/DataBox';
-import Select from './components/select/Select'
-import NavBar from './components/NavBar';
-
-// import DataGraph from './components/DataGraph';
-
-import {useState, useEffect} from 'react';
-import axios from 'axios';
+import logo from './logo.svg';
 import './App.css';
-// Bootstrap 
-import 'bootstrap/dist/css/bootstrap.min.css';
-// May not need these:
-// import 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css';
-// import 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css';
-// FontAwesome
-// Errored:------> import 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/themes/base/jquery-ui.min.css';
-// Errored:------> import 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Staatliches&display=swap';
-// Errored:------> import 'https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css';
 
-async function fetchCurrentData() {
-  // console.log("data called");
-  const response = await axios.get("https://api.covidtracking.com/v1/us/current.json");
-  // console.log(response);
-  return response.data;
-}
 function App() {
-  const [ currentData, updateCurrentState ] = useState([]);
-
-  useEffect(async ()=>{
-    // console.log(data)
-    const data = await fetchCurrentData();
-    updateCurrentState(data)
-  }, 
-  [])
-  // console.log(currentData[0].date);
-  return (  
-  <div>
-      <header className="card-header p-3">
-      {/* ? = then show data boxes
-      if 0 will go to null */}
-        {
-          currentData.length ? 
-          <>
-          <NavBar/>
-            </>
-          : null
-        }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <div className="container w-auto p-3">
-      {/* // Insert Card import here */}
-      {
-          currentData.length ?
-          <>
-      <Select/>
-            </>
-          : null
-        }
-      </div>
-      <main className="m-5 row justify-content-around">
-        {
-          currentData.length ?
-          <>
-            <DataBox cases={currentData[0].date}/>          
-            <DataBox/>
-            <DataBox/>
-            <DataBox/>
-            </>
-          : null
-        }
-      </main>
-      <body className="m-5 row justify-content-around">
-        {
-          currentData.length ?
-          <>
-            <DataBox cases={currentData[0].date}/>          
-            <DataBox/>
-            <DataBox/>
-            <DataBox/>
-            </>
-          : null
-        }
-      </body>
-  </div>
+    </div>
   );
 }
 
